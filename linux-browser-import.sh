056e165469e700cadb549df241701d1568278c1f
Thomas Leister <thomas.leister@mailbox.org>
2017-03-09 09:47:28 +0100

    Update linux-browser-import.sh

--- linux-browser-import.sh
+++ linux-browser-import.sh
@@ -21,7 +21,7 @@ certname="My Root CA"
 ### For cert8 (legacy - DBM)
 ###
 
-for certDB in $(find ~/ -name "cert8.db")
+for certDB in $(find ~/ -writable -name "cert8.db")
 do
     certdir=$(dirname ${certDB});
     certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d dbm:${certdir}
@@ -32,7 +32,7 @@ done
 ### For cert9 (SQL)
 ###
 
-for certDB in $(find ~/ -name "cert9.db")
+for certDB in $(find ~/ -writable -name "cert9.db")
 do
     certdir=$(dirname ${certDB});
     certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
